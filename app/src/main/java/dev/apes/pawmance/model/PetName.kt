a0aba3230f877eb946df32746d1425213838a59e
strongforce1 <fporquillo18@gmail.com>
2021-12-07 23:45:41 +0800

    Add additional pet attributes

--- app/src/main/java/dev/apes/pawmance/model/PetName.kt
+++ app/src/main/java/dev/apes/pawmance/model/PetName.kt
@@ -3,5 +3,8 @@ package dev.apes.pawmance.model
 data class PetName(
   val id: String,
   val name: String,
-  val breed: String
+  val breed: String? = null,
+  val birthdate: String? = null,
+  val preference: String? = null,
+  val gender: String? = null
 )
