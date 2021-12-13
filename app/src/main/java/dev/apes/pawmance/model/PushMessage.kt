5ade112eead3d9b9d8b962f959862694228ad5c8
strongforce1 <fporquillo18@gmail.com>
2021-12-13 23:53:24 +0800

    Update PushMessage.kt

--- app/src/main/java/dev/apes/pawmance/model/PushMessage.kt
+++ app/src/main/java/dev/apes/pawmance/model/PushMessage.kt
@@ -2,5 +2,10 @@ package dev.apes.pawmance.model
 
 data class PushMessage(
   val to: String,
-  val data: MessageData
+  val data: MessageDataV2
+)
+
+data class MessageDataV2(
+  val matchName: String? = null,
+  val youLike: String? = null
 )
