da8fa26d36e92c7d05c5825eda5ffed0076c7ca2
strongforce1 <fporquillo18@gmail.com>
2021-12-13 23:53:31 +0800

    Update Conversations.kt

--- app/src/main/java/dev/apes/pawmance/model/Conversations.kt
+++ app/src/main/java/dev/apes/pawmance/model/Conversations.kt
@@ -3,13 +3,14 @@ package dev.apes.pawmance.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import dev.apes.pawmance.utils.TimeUtils
+import java.util.Calendar
 
 @Entity
 data class Conversations(
   val content: String,
   val petName: String,
   val petId: String,
-  val timestamp: Long = TimeUtils.instantTimeLocalize(),
+  val timestamp: Long = Calendar.getInstance().timeInMillis,
   val fromSender: Boolean,
   val recipientId: String,
 
