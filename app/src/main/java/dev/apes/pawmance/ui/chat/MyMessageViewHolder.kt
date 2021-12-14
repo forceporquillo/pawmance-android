f5ce52ec757b49a907ea29ade0605037d8d1dda5
strongforce1 <fporquillo18@gmail.com>
2021-12-14 22:58:21 +0800

    Update MyMessageViewHolder.kt

--- app/src/main/java/dev/apes/pawmance/ui/chat/MyMessageViewHolder.kt
+++ app/src/main/java/dev/apes/pawmance/ui/chat/MyMessageViewHolder.kt
@@ -2,12 +2,15 @@ package dev.apes.pawmance.ui.chat
 
 import com.devforcecodes.pawmance.databinding.MyMessageBinding
 import dev.apes.pawmance.model.ConversationUiModel
+import dev.apes.pawmance.model.ConversationUiModel.SenderUiModel
+import timber.log.Timber
 
 class MyMessageViewHolder(
   private val binding: MyMessageBinding
 ) : ConversationViewHolder(binding.root) {
 
   override fun bindMessage(conversations: ConversationUiModel) {
+    //binding.date.text = conversations.timestamp
     binding.myMessageText.text = conversations.content
   }
 }
