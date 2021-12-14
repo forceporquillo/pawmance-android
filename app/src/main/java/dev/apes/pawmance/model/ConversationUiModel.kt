edb45fe8252205cc248835c926bb7aeb92fb00db
strongforce1 <fporquillo18@gmail.com>
2021-12-14 22:58:10 +0800

    Update ConversationUiModel.kt

--- app/src/main/java/dev/apes/pawmance/model/ConversationUiModel.kt
+++ app/src/main/java/dev/apes/pawmance/model/ConversationUiModel.kt
@@ -1,11 +1,37 @@
 package dev.apes.pawmance.model
 
-data class ConversationUiModel(
-  val content: String,
-  val petName: String,
-  val petId: String,
-  val fromSender: Boolean,
-  val recipientId: String,
-  val id: Int,
-  val petPetProfile: String? = null
-)
+
+
+sealed class ConversationUiModel {
+  open val content: String = ""
+  open val petName: String = ""
+  open val petId: String = ""
+  open val petPetProfile: String? = null
+  open val recipientId: String = ""
+  open val id: Int = -1
+  open val timestamp: String = ""
+
+  data class ReceivedUiModel(
+    override val content: String,
+    override val petId: String,
+    override val petName: String,
+    override val petPetProfile: String?,
+    override val id: Int,
+    override val recipientId: String,
+    override val timestamp: String
+  ): ConversationUiModel()
+
+  data class SenderUiModel(
+    override val content: String,
+    override val petId: String,
+    override val petName: String,
+    override val petPetProfile: String?,
+    override val id: Int,
+    override val recipientId: String,
+    override val timestamp: String
+  ): ConversationUiModel()
+
+  data class DateHeaderUiModel(
+    override val timestamp: String
+  ): ConversationUiModel()
+}
