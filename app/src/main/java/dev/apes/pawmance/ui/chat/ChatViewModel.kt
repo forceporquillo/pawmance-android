19182306c09f78cfbd2775b4efa67aeeca00fca1
strongforce1 <fporquillo18@gmail.com>
2021-12-14 22:58:25 +0800

    Update ChatViewModel.kt

--- app/src/main/java/dev/apes/pawmance/ui/chat/ChatViewModel.kt
+++ app/src/main/java/dev/apes/pawmance/ui/chat/ChatViewModel.kt
@@ -6,7 +6,9 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.apes.pawmance.data.conversation.ConversationUseCase
 import dev.apes.pawmance.model.ChatItems
 import dev.apes.pawmance.utils.successOr
+import io.getstream.chat.android.offline.ChatDomain
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -14,11 +16,14 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ChatViewModel @Inject constructor(
-  private val conversationUseCase: ConversationUseCase
+  private val conversationUseCase: ConversationUseCase,
+  chatDomain: ChatDomain
 ) : ViewModel() {
 
   private val _recentConversations = MutableStateFlow<List<ChatItems>>(emptyList())
   val recentConversations = _recentConversations.asStateFlow()
+
+  val totalUnreadCount: StateFlow<Int> = chatDomain.totalUnreadCount
 
   init {
     viewModelScope.launch {
