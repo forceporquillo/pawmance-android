3d8aa73657ae58d3359f4eb5df2483eaa2ec5c82
strongforce1 <fporquillo18@gmail.com>
2021-12-15 22:53:53 +0800

    Update FirebaseMessageService.kt

--- app/src/main/java/dev/apes/pawmance/data/fcm/FirebaseMessageService.kt
+++ app/src/main/java/dev/apes/pawmance/data/fcm/FirebaseMessageService.kt
@@ -5,7 +5,7 @@ import com.google.firebase.messaging.RemoteMessage
 import dagger.hilt.android.AndroidEntryPoint
 import dev.apes.pawmance.data.conversation.ConversationsDao
 import dev.apes.pawmance.model.Conversations
-import dev.apes.pawmance.utils.TimeUtils
+import io.karn.notify.Notify
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -23,28 +23,41 @@ class FirebaseMessageService : FirebaseMessagingService() {
   override fun onMessageReceived(remoteMessage: RemoteMessage) {
     super.onMessageReceived(remoteMessage)
 
-    val conversations = remoteMessage.mapToConversation()
+    remoteMessage.data["matchName"]?.let { name ->
+      showMatchNotifications(
+        header = {
+          "Wow! You have a new match! ❤\uD83D\uDE0D"
+        },
+        matchName = {
+          "You and $name both liked each other! \uD83D\uDC36"
+        }
+      )
+    }
 
-    conversationsDao.insertMessage(conversations)
-
-    Timber.e("Message received ${remoteMessage.senderId} content: ${remoteMessage.data["content"]}")
+    remoteMessage.data["youLike"]?.let { name ->
+      showMatchNotifications(
+        header = {
+          "Someone likes your profile! \uD83D\uDC4D"
+        },
+        matchName = {
+          "$name likes you, Swipe to see now! ❤\uD83D\uDE0D"
+        }
+      )
+    }
   }
 
-  private fun RemoteMessage.mapToConversation(): Conversations {
-    val content = data["content"] as String
-    val petName = data["petName"] as String
-    val petId = data["petId"] as String
-    val timeStamp = data["timeStamp"]
-    val recipientId = data["recipientId"] as String
-
-    return Conversations(
-      content,
-      petName,
-      recipientId,
-      timeStamp?.toLong()
-        ?: TimeUtils.instantTimeLocalize(),
-      true,
-      petId
-    )
+  private inline fun showMatchNotifications(
+    crossinline header: () -> String,
+    crossinline matchName: () -> String
+  ) {
+    Notify
+      .with(applicationContext)
+      .content { // this: Payload.Content.Default
+        // The title of the notification (first line).
+        title = header.invoke()
+        // The second line of the notification.
+        text = matchName.invoke()
+      }
+      .show()
   }
 }
